a8195443a27a4681b7e2f4fdd5df69a3e67171a2
Clarity Shimoniak <clarity.shimoniak@email.ucr.edu>
2023-04-17 21:08:08 -0700

    LED Blink template

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+
+const uint8_t LED_PIN = PICO_DEFAULT_LED_PIN;
+
+
+int main() {
+	gpio_init(LED_PIN);
+	gpio_set_dir(LED_PIN, GPIO_OUT);
+	
+	while (true) {
+		gpio_put(LED_PIN, 1);
+		sleep_ms(250);
+		gpio_put(LED_PIN, 0);
+		sleep_ms(250);
+	}
+	
+	return 0;
+}
