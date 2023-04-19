1b3078837ca0d4ee7931be9817771e30b35cb960
Clarity Shimoniak <clarity.shimoniak@email.ucr.edu>
2023-04-19 11:14:16 -0700

    USB printf output instead of UART
    Defaults to /dev/ttyACM0 instead of /dev/ttyUSB0 like the FTDI does

--- main.c
+++ main.c
@@ -6,13 +6,17 @@ const uint8_t LED_PIN = PICO_DEFAULT_LED_PIN;
 
 
 int main() {
+	stdio_init_all();
 	gpio_init(LED_PIN);
 	gpio_set_dir(LED_PIN, GPIO_OUT);
 	
 	while (true) {
 		gpio_put(LED_PIN, 1);
+		printf("On\n");
 		sleep_ms(250);
+		
 		gpio_put(LED_PIN, 0);
+		printf("Off\n");
 		sleep_ms(250);
 	}
 	
